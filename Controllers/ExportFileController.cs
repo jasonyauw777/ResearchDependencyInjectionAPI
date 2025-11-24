fec36daf853dd88ea297b6a1c9bab51612e0041f
Jason Surya <Jason_p.p@bi.go.id>
2025-11-24 15:23:54 +0700

    refactor to use one endpoint

---
+++ Controllers/ExportFileController.cs
@@ -0,0 +1,25 @@
+﻿using DependencyInjectionAPI.Factory;
+using DependencyInjectionAPI.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjectionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExportFileController
+    {
+        private readonly ExportFileServiceFactory _exportFileServiceFactory;
+
+        public ExportFileController(ExportFileServiceFactory exportFileServiceFactory)
+        {
+            _exportFileServiceFactory = exportFileServiceFactory;
+        }
+
+        [HttpGet("{fileType}")]
+        public string Export(string fileType)
+        {
+            IExportFileService _exportFileService = _exportFileServiceFactory(fileType);
+            return _exportFileService.ExportFile("Filename");
+        }
+    }
+}
